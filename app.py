57f8e8b07f55ba6fc536b5dd721b025c5d14a77e
Orinda <orindatt@gmail.com>
2025-02-24 22:35:39 +0100

    Initial commit with Flask app, Dockerfile, and docker-compose.yml

--- app.py
+++ app.py
@@ -1,52 +1,37 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template
+import pymysql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@db:3306/your_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# MySQL configurations
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'your_db'
 
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(15), nullable=False)
-
-def initialize_database():
-    with app.app_context():
-        db.create_all()
-
-
-initialize_database()
-
-
-@app.route('/add', methods=['POST'])
-def add_user():
-    data = request.json
-    email = data.get('email')
-    phone = data.get('phone')
-
-    if not email or not phone:
-        return jsonify({"error": "Email and phone are required"}), 400
-
-    new_user = User(email=email, phone=phone)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "User added successfully"}), 201
-
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    user_list = [{"id": user.id, "email": user.email, "phone": user.phone} for user in users]
-    return jsonify(user_list), 200
-
+def get_db_connection():
+    return pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
 
 @app.route('/')
 def index():
-    return "Welcome to the Flask Dockerized App!"
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    email = request.form['email']
+    phone = request.form['phone']
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (email, phone) VALUES (%s, %s)", (email, phone))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return "Data added successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
